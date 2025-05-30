1156ebd590d4041803f457054085e6ce014a3f0d
Q Misell <q@magicalcodewit.ch>
2025-05-30 21:18:22 +0200

    do not init barkoder config every time

--- main/aztec.py
+++ main/aztec.py
@@ -2,34 +2,13 @@ import numpy as np
 import cv2
 from django.conf import settings
 
-
-class AztecError(Exception):
-    pass
-
-
-def decode(img_data: bytes, *, scan_speed: str = "slow"):
-    try:
-        import Barkoder
-    except ImportError as e:
-        raise AztecError("Barkoder SDK not available") from e
-
-    cfg_response = Barkoder.Config.InitializeWithLicenseKey(settings.BARKODER_LICENSE)
-    assert cfg_response.get_result() == Barkoder.ConfigResult.OK
-    config = cfg_response.get_config()
-
-    config.encodingCharacterSet = "BINARY"
-
-    match scan_speed.lower():
-        case "slow":
-            config.decodingSpeed = Barkoder.DecodingSpeed.Slow
-        case "normal":
-            config.decodingSpeed = Barkoder.DecodingSpeed.Normal
-        case "fast":
-            config.decodingSpeed = Barkoder.DecodingSpeed.Fast
-        case _:
-            config.decodingSpeed = Barkoder.DecodingSpeed.Slow
-
-    assert config.set_enabled_decoders([
+try:
+    import Barkoder
+    barkoder_cfg_response = Barkoder.Config.InitializeWithLicenseKey(settings.BARKODER_LICENSE)
+    assert barkoder_cfg_response.get_result() == Barkoder.ConfigResult.OK
+    barkoder_config = barkoder_cfg_response.get_config()
+    barkoder_config.encodingCharacterSet = "BINARY"
+    assert barkoder_config.set_enabled_decoders([
         Barkoder.DecoderType.Aztec,
         Barkoder.DecoderType.AztecCompact,
         Barkoder.DecoderType.QR,
@@ -37,15 +16,37 @@ def decode(img_data: bytes, *, scan_speed: str = "slow"):
         Barkoder.DecoderType.PDF417,
         Barkoder.DecoderType.PDF417Micro,
     ]).get_result() == Barkoder.ConfigResult.OK
+except ImportError as e:
+    barkoder_config = None
+
+class AztecError(Exception):
+    pass
+
+def decode(img_data: bytes, *, scan_speed: str = "slow"):
+    if not barkoder_config:
+        raise AztecError("Barkoder SDK not available") from e
+
+    match scan_speed.lower():
+        case "slow":
+            barkoder_config.decodingSpeed = Barkoder.DecodingSpeed.Slow
+        case "normal":
+            barkoder_config.decodingSpeed = Barkoder.DecodingSpeed.Normal
+        case "fast":
+            barkoder_config.decodingSpeed = Barkoder.DecodingSpeed.Fast
+        case _:
+            barkoder_config.decodingSpeed = Barkoder.DecodingSpeed.Slow
 
     img = cv2.imdecode(np.asarray(bytearray(img_data), dtype="uint8"), cv2.IMREAD_GRAYSCALE)
     if img is None:
         raise AztecError("Unable to read image")
 
     height, width = img.shape[:2]
-    results = Barkoder.Barkoder.DecodeImageMemory(config, img, width, height)
+    results = Barkoder.Barkoder.DecodeImageMemory(barkoder_config, img, width, height)
 
     if len(results) > 0:
-        return bytes(results[0].binaryData)
+        d = bytes(results[0].binaryData)
+        del results
+        return d
     else:
+        del results
         raise AztecError("No barcodes found")
