52faece74a0696b93a47c5888a119a3a0693ef2a
Q Misell <q@magicalcodewit.ch>
2025-09-10 13:00:43 +0200

    remove ÖBB GTFS

--- gtfs/data.py
+++ gtfs/data.py
@@ -1,6 +1,6 @@
 GTFS_FEEDS = {
     "eil": "https://integration-storage.dm.eurostar.com/gtfs-prod/gtfs_static_commercial_v2.zip",
-    "oebb": "https://static.web.oebb.at/open-data/soll-fahrplan-gtfs/GTFS_OP_2025_obb.zip",
+#    "oebb": "https://static.web.oebb.at/open-data/soll-fahrplan-gtfs/GTFS_OP_2025_obb.zip",
 }
 GTFS_RT_FEEDS = {
     "eil": "https://integration-storage.dm.eurostar.com/gtfs-prod/gtfs_rt_v2.json"
