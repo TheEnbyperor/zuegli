30d7d98470278d571cd496a1898c6a2aeb1ca9ad
Q Misell <q@magicalcodewit.ch>
2025-10-16 14:34:48 +0200

    set isolation level, take 3

--- vdv_pkpass/settings.py
+++ vdv_pkpass/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "PORT": '26257',
         "OPTIONS": {
             "application_name": os.getenv("APP_NAME", "zuegli"),
-            "-c": "default_transaction_isolation=read\ committed",
+            "isolation_level": "read committed",
         }
     }
 }
